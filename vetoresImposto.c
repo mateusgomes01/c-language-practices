fb59b7ff37630bbbe25496b504debc2dec99dadc
Mateus Gomes <mateusmelo500@gmail.com>
2019-11-16 19:13:55 -0300

    Update vetoresImposto.c
    Finished the question

--- vetoresImposto.c
+++ vetoresImposto.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /*
-crie um programa que le os salarios de um funcionario ao longo do ano,
-e tambem le um imposto para ser aplicado em cada salario. O imposto deve
+crie um programa que le um imposto e, em seguida, le os salarios de um funcionario ao longo do ano,
+e em seguida deve aplicar este imposto a cada salario. O imposto deve
 estar entre 0 e 100 (inclusos). Caso o usuario digite um valor fora da faixa,
 eh necessario ler novamente. No final, exibe o valor do salario correspondente a cada mes
 e o salario anual total
@@ -10,15 +10,28 @@ e o salario anual total
 
 int main(){
 	int i;
-	float salario[12];
+	float salario[12], somatorio, imposto;
 	
-	for( i=0 ; i<12 ; i++ ){
-		scanf("%f", &salario[i]);		
+    printf("Digite o imposto a ser aplicado. Deve estar entre 0 e 100: ");
+  	scanf("%f", &imposto);
+  
+  	while( imposto<0 || imposto>100 ){
+    	printf("Valor invalido! Digite um valor entre 0 e 100!");
+    	scanf("%f", &imposto); 
+  	}
+  
+	for( i=0, somatorio=0 ; i<12 ; i++ ){
+		printf("Digite o salario do mes %d: ", i);
+		scanf("%f", &salario[i]);
+    	salario[i] = salario[i] * (1 - (imposto/100));
+    	somatorio = somatorio + salario[i];
 	}
 	
 	for( i=0 ; i<12 ; i++ ){
-		printf("Salario mes %d: %f\n", i, salario[i]);		
+		printf("Salario mes %d: %f\n", i, salario[i]);
 	}
+	
+	printf("Total anual: %f\n", somatorio);
 	
 	return 0;
 }
